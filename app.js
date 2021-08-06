ebc1be6849c72f0ec2771b908b4f815248258b51
Tuba <tadiguzel96@gmail.com>
2021-08-06 13:16:45 +0300

    Staticfiles added

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const ejs = require('ejs')
 const app = express()
 
+app.set("view engine","ejs")
+app.use(express.static("public"))
+
 app.get("/", (req,res) =>{
-    res.send("Index sayfası")
+    res.status(200).render("index",{
+        page_name: "index"
+    })
+})
+
+app.get("/about", (req,res) =>{
+    res.status(200).render("about",{
+        page_name: "about"
+    })
 })
 
 const PORT = 3000
