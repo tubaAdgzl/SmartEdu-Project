fa5382f4938834539faf37ba03ec0603a3079b5c
Tuba <tadiguzel96@gmail.com>
2021-08-28 12:32:39 +0300

    Contact Form added

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,10 +22,9 @@ exports.loginUser = async (req,res) =>{
        await User.findOne({email:email},(err,user) =>{
            if(user){
                 bcrypt.compare(password,user.password,(err,same) =>{
-                    if(same){
+                   
                         req.session.userID = user._id;
                         res.status(200).redirect('dashboard')
-                    }
                 })
            }
        })
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -10,7 +10,7 @@ exports.createCourse = async (req,res) =>{
           user:req.session.userID
       });
       res.status(201).redirect("/courses")
-   }catch{
+   }catch(error){
        res.status(400).json({
            status:"fail",
            error
@@ -22,13 +22,30 @@ exports.getAllCourses = async (req,res) =>{
     try{
 
         const categorySlug = req.query.categories;
+        const query = req.query.search;
+
         const category = await Category.findOne({slug:categorySlug})
     
         let filter = {};
         if(categorySlug) {
           filter = {category:category._id}
         }
-        const courses = await Course.find(filter).sort("-createdAt");
+
+        if(query){
+            filter = {name:query}
+        }
+
+        if(!query && !categorySlug) {
+            filter.name = "",
+            filter.category = null
+        }
+        const courses = await Course.find({
+            $or:[
+                {name: { $regex: '.*' + filter.name + '.*', $options:'i'}},
+                {category: filter.category}
+            ]
+        }).sort("-createdAt").populate("user")
+
         const categories = await Category.find()
         res.status(200).render('courses',{
             courses,
@@ -45,13 +62,48 @@ exports.getAllCourses = async (req,res) =>{
 
 exports.getCourse = async (req,res) =>{
     try{
+        const user = await User.findById(req.session.userID);
         const course = await Course.findOne({slug:req.params.slug}).populate('user')
+       
+        const categories = await Category.find()
+       
         res.status(200).render("course",{
             course,
-            page_name:"courses"
+            page_name:"courses",
+            user,
+            categories
         })
     }catch(error){
         res.status(404).json({
+            status:"fail",
+            error,
+        })
+    }
+}
+
+exports.enrollCourse = async (req,res) =>{
+    try{
+        const user = await User.findById(req.session.userID)
+        await user.courses.push({_id:req.body.course_id})
+        await user.save()
+        res.status(200).redirect("/users/dashboard")
+    }catch(error){
+        res.status(400).json({
+            status:"fail",
+            error        
+        })
+    }
+}
+
+
+exports.releaseCourse = async (req,res) =>{
+    try{
+        const user = await User.findById(req.session.userID)
+        await user.courses.pull({_id:req.body.course_id})
+        await user.save()
+        res.status(200).redirect('/users/dashboard')
+    }catch(error){
+        res.status(400).json({
             status:"fail",
             error        
         })
